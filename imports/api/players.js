33472f8f3b1f7b6af9903bafbe66631c44f88fc4
Brad Koch <brad@farmlogs.com>
2018-08-25 10:30:25 -0500

    Add 2017 data

--- imports/api/players.js
+++ imports/api/players.js
@@ -1,14 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
-import { raw_players } from '../data/players.js'
+import { raw_players as players_2016 } from '../data/players.2016.js'
+import { raw_players as players_2017 } from '../data/players.2017.js'
  
 export const Players = new Mongo.Collection('players');
  
 Meteor.methods({
   'players.reset'() {
     Players.remove({})
-    for (let player of raw_players) {
+    for (let player of players_2017) {
       Players.insert(player);
     }
   },
